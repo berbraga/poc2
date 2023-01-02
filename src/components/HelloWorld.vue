422d3b34948d8f17a17b58b039266ace6fe71d8d
berbraga <bernardo.sbraga@yahoo.com>
2023-01-02 09:59:04 -0300

    Co-authored-by: Gugafss <Gugafss@users.noreply.github.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,22 @@
 <template>
   <div>
     <h1 style='font-size: 3.2em; font-family: cursive;'><b>Agora.io</b></h1>
-    <button @click="this.enterProfessional()" style='width: 200px; margin: 10px'>Entrar/Fechar Profissional</button>
-    <button @click="this.enterClient()" style='width: 200px'>Entrar/Fechar Cliente</button>
+    <button @click="this.enterVideo()" style='width: 200px'>Criar um Canal</button>
+    <button @click="this.joinChannel()" style='width: 200px'>Entrar no Canal</button>
+    <button @click="this.leaveChannel()" style='width: 200px'>Sair do Canal</button>
   </div>
 
   <div v-if="this.open === true">
     <div class="outside">
       <div class="player">
-        <!-- player do video -->
-        <iframe width="1280" height="720" src="https://www.youtube.com/embed/KCc4TEbZCmc" title="O TRANSMISSOR de RÁDIO MAIS SIMPLES do MUNDO!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <!-- <iframe width="1280" height="720" src="https://www.youtube.com/embed/KCc4TEbZCmc" title="O TRANSMISSOR de RÁDIO MAIS SIMPLES do MUNDO!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
         <div class="professional">
-          <!-- player DA CAMERA do PROFISSIONAL -->
-          <video id="video"> </video>
+          <div id="video"></div>
         </div>
       </div>
     </div>
   </div>
-  <div v-else style="display:none;">
-    <div class="outside">
-      <div class="player">
-        <!-- player do video -->
-        <div class="professional">
-          <!-- player DA CAMERA do PROFISSIONAL -->
-          <video id="video"> </video>
-        </div>
-      </div>
-    </div>
-  </div>
+
 </template>
 
 <script>
@@ -45,17 +34,9 @@ export default {
       appId: 'b5cd1cffff564ab5a4dbf08d3e7d59b1',
       appKey:'007eJxTYHB5cOLCybbcvtTTk1MF/l2VvL48S/7JqrW/ove6fromEvpQgSHJNDnFMDkNCEzNTBKTTBNNUpLSDCxSjFPNU0wtkwyPL1ua3BDIyKB9JZuVkQECQXwWhoD8ZCMGBgChnyPc',
       channelName: 'Poc2',
-// const AgoraRTC = require('agora-rtc-sdk'); const appId = "YOUR_APP_ID"; const appKey = "YOUR_APP_KEY";
-
-
-      localStreams: {
-        uid: '',
-        camera: {
-          camId: '',
-          midId: '',
-          stream: {},
-        }
-      },
+      client: AgoraRTC.createClient({ mode: 'rtc', codec: 'h264' }),
+      localStream: {},
+      // localStream: AgoraRTC.createStream({streamID: uid, audio: true, video: true, screen: false}),
 
       devices: {
         cameras: [],
@@ -66,236 +47,163 @@ export default {
       injectedStreamURL: '',
 
       defaultConfigRTMP: {
-      width: 640,
-      height: 360,
-      videoBitrate: 400,
-      videoFramerate: 15,
-      lowLatency: false,
-      audioSampleRate: 48000,
-      audioBitrate: 48,
-      audioChannels: 1,
-      videoGop: 30,
-      videoCodecProfile: 100,
-      userCount: 0,
-      userConfigExtraInfo: {},
-      backgroundColor: 0x000000,
-      transcodingUsers: [],
+        width: 640,
+        height: 360,
+        videoBitrate: 400,
+        videoFramerate: 15,
+        lowLatency: false,
+        audioSampleRate: 48000,
+        audioBitrate: 48,
+        audioChannels: 1,
+        videoGop: 30,
+        videoCodecProfile: 100,
+        userCount: 0,
+        userConfigExtraInfo: {},
+        backgroundColor: 0x000000,
+        transcodingUsers: [],
       },
 
     }
   },
 
-  methods: {
+methods: {
+  enterVideo() {
+    this.open= !this.open
 
-    enterProfessional() {
-      this.open= !this.open
-    navigator.mediaDevices.getUserMedia({video: true})
-    .then(function (mediaStream) {
-      const video = document.querySelector('#video');
-      video.srcObject = mediaStream;
-      video.play();
-    })
-    .catch(function (err) {
-      console.log('Não há permissões para acessar a webcam')
-    })
+    this.client.init(this.appId, () =>
+      console.log('AgoraRTC this.client initialized'));
+      let uidId = 0;
+    this.client.join(this.appKey, this.channelName, null, (uid) => {
+      uidId = uid;
+      this.localStream = AgoraRTC.createStream({streamID: uid, audio: true, video: true, screen: false})
+      console.log(`User ${uid} joined channel`);
+    });
+
+
+    this.localStream.init(() => {
+      console.log('Local stream initialized');
+      this.localStream.play('video');
+      this.client.publish(this.localStream, (err) =>
+      console.log('Publish local stream error: ' + err));
+    });
+
+    // Publish the local stream
+    this.client.publish(this.localStream, function (err) {
+      console.log("publish failed");
+      console.error(err);
+    });
+
+    this.client.on('stream-added', (evt) => {
+      // const stream = evt.stream;
+      var remoteStream = evt.stream;
+      var id = remoteStream.getId();
+      if (id !== uidId) {
+        this.client.subscribe(remoteStream, function (err) {
+          console.log("stream subscribe failed", err);
+        });
+      }
+      console.log("stream-added remote-uid: ", id);
+      console.log(`Stream added:${evt}`)
+    });
+
+    this.client.on("stream-subscribed", function (evt) {
+      var remoteStream = evt.stream;
+      var id = remoteStream.getId();
+      // Add a view for the remote stream.
+      let streamDiv =  document.createElement("div"); // Create a new div for every stream
+      streamDiv.id = id;                            // Assigning id to div
+      streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
+      remoteContainer.appendChild(streamDiv);
+      // Play the remote stream.
+      remoteStream.play("remote_video_" + id);
+      console.log("stream-subscribed remote-uid: ", id);
+    });
   },
 
+  joinChannel() {
+    var token = generateToken();
+    var userID = null; // set to null to auto generate uid on successfull connection
+    // set the role
+    this.client.setClientRole('host', function() {
+      console.log('this.Client role set as host.');
+    }, function(e) {
+      console.log('setthis.ClientRole failed', e);
+    });
 
-    enterClient() {
-      this.open= !this.open
+    // this.client.join(token, 'allThingsRTCLiveStream', 0, function(uid) {
+    this.client.join(token, channelName, userID, function(uid) {
+        createCameraStream(uid, {});
+        this.localStreams.uid = uid; // keep track of the stream uid
+        console.log('User ' + uid + ' joined channel successfully');
+    }, function(err) {
+        console.log('[ERROR] : join channel failed', err);
+    });
+  },
 
-      const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+// video streams for channel
+     createCameraStream(uid, deviceIds) {
+      console.log('Creating stream with sources: ' + JSON.stringify(deviceIds));
 
-      client.init(this.appId, () =>
-        console.log('AgoraRTC client initialized'));
+      this.localStream.setVideoProfile(cameraVideoProfile);
 
-
-      client.join(this.appKey, this.channelName, null, (uid) => {
-        console.log('================PASSOU ======================')
-        var uidId = uid;
-        console.log(`User ${uid} joined channel`);
+      // The user has granted access to the camera and mic.
+      this.localStream.on("accessAllowed", function() {
+        if(devices.cameras.length === 0 && devices.mics.length === 0) {
+          console.log('[DEBUG] : checking for cameras & mics');
+          getCameraDevices();
+          getMicDevices();
+        }
+        console.log("accessAllowed");
       });
-      console.log(uidId)
-      const localStream = AgoraRTC.createStream({ streamID: uidId, audio: true, video: true, screen: false });
-      console.log('========== ANTES LOCAL STREAM ==========');
-
-      localStream.init(() => {
-        console.log('Local stream initialized');
-        console.log('==========entrou ==========');
-
-        localStream.play('video');
-        console.log('==========tyocando no video==========');
-        client.publish(localStream, (err) =>
-        console.log('Publish local stream error: ' + err));
+      // The user has denied access to the camera and mic.
+      this.localStream.on("accessDenied", function() {
+        console.log("accessDenied");
       });
 
-      client.on('stream-added', (evt) => {
-        const stream = evt.stream;
-        console.log(`Stream added:${evt}`)
+      this.localStream.init(function() {
+        console.log('getUserMedia successfully');
+        this.localStream.play('full-screen-video'); // play the local stream on the main div
+        // publish local stream
 
+        if($.isEmptyObject(this.localStreams.camera.stream)) {
+          enableUiControls(this.localStream); // move after testing
+        } else {
+          //reset controls
+          $("#mic-btn").prop("disabled", false);
+          $("#video-btn").prop("disabled", false);
+          $("#exit-btn").prop("disabled", false);
+        }
+
+        this.client.publish(this.localStream, function (err) {
+          console.log('[ERROR] : publish local stream error: ' + err);
+        });
+
+        localStreams.camera.stream = this.localStream; // keep track of the camera stream for later
+      }, function (err) {
+        console.log('[ERROR] : getUserMedia failed', err);
       });
-
-      // client.init(this.agoraAppId, function() {
-      //   //console.log('===========1==============')
-  		// 	var userID = null; // set to null to auto generate uid on successfull connection
-      //   //console.log('===========2==============')
-
-  		// 	client.setClientRole('host', function() {
-      //  //console.log('===========3==============')
-  		// 		console.log('Client role set as host.');
-  		// 	}, function(e) {
-      //   //console.log('===========4==============')
-  		// 		console.log('setClientRole failed', e);
-  		// 	});
-      //   let channel = 'poc2'
-
-  		// 	// this.localStreams.uid = uid; // keep track of the stream uid
-
-
-
-
-
-      //   console.log('===========5==============')
-
-  		// 	client.join(null,  channel, null, function(uid) {
-
-      //   console.log('===========5.5==============')
-  		// 			//createCameraStream(uid, {});
-      //   console.log('===========6==============')
-      //   console.log('===========7==============')
-  		// 	console.log('User ' + uid + ' joined channel successfully');
-  		// 	}, function(err) {
-      //   console.log('=========== ERRO ==============')
-  		// 			console.log('[ERROR] : join channel failed', err);
-  		// 	});
-      // }, function(err) {
-      //   console.log('[ERROR] : AgoraRTC client init failed', err);
-      // });
-
     },
 
-    // NAO TA FUNCIONANDO A PARTE DE IR PARA ESSA FUNCAO!!
-
-	  // joinChannel() {
-		// 	var userID = null; // set to null to auto generate uid on successfull connection
-		// 	console.log('teste Agora Agora2');
-		// 	// set the role
-		// 	client.setClientRole('host', function() {
-		// 		console.log('Client role set as host.');
-		// 	}, function(e) {
-		// 		console.log('setClientRole failed', e);
-		// 	});
-		// 	// client.join('allThingsRTCLiveStream', 0, function(uid) {
-		// 	client.join(channelName, userID, function(uid) {
-		// 			createCameraStream(uid, {});
-		// 			localStreams.uid = uid; // keep track of the stream uid
-		// 	console.log('User ' + uid + ' joined channel successfully');
-		// 	}, function(err) {
-		// 			console.log('[ERROR] : join channel failed', err);
-		// 	});
-		// },
-
-    // createCameraStream (uid, deviceIds) {
-		// 	console.log('Creating stream with sources: ' + JSON.stringify(deviceIds));
-
-    //   var localStream = AgoraRTC.createStream({
-    //     streamID: uid,
-    //     audio: true,
-    //     video: true,
-    //     screen: false
-    //   });
-    //   localStream.setVideoProfile(cameraVideoProfile);
-
-    //   // The user has granted access to the camera and mic.
-    //   localStream.on("accessAllowed", function() {
-    //     if(devices.cameras.length === 0 && devices.mics.length === 0) {
-    //       console.log('[DEBUG] : checking for cameras & mics');
-    //       getCameraDevices();
-    //       getMicDevices();
-    //     }
-    //     console.log("accessAllowed");
-    //   });
-    //   // The user has denied access to the camera and mic.
-    //   localStream.on("accessDenied", function() {
-    //     console.log("accessDenied");
-    //   });
-
-    //   localStream.init(function() {
-    //     console.log('getUserMedia successfully');
-    //     localStream.play('full-screen-video'); // play the local stream on the main div
-    //     // publish local stream
-
-    //     if($.isEmptyObject(localStreams.camera.stream)) {
-    //       enableUiControls(localStream); // move after testing
-    //     } else {
-    //       //reset controls
-    //       $("#mic-btn").prop("disabled", false);
-    //       $("#video-btn").prop("disabled", false);
-    //       $("#exit-btn").prop("disabled", false);
-    //     }
-
-    //     client.publish(localStream, function (err) {
-    //       console.log('[ERROR] : publish local stream error: ' + err);
-    //     });
-
-    //     localStreams.camera.stream = localStream; // keep track of the camera stream for later
-    //   }, function (err) {
-		// 		console.log('[ERROR] : getUserMedia failed', err);
-    //   });
-	  // },
 
 
-//     createCameraStream(uid, deviceIds) {
-//     console.log('Creating stream with sources: ' + JSON.stringify(deviceIds));
-
-//     var localStream = AgoraRTC.createStream({
-//       streamID: uid,
-//       audio: true,
-//       video: true,
-//       screen: false
-//     });
-//     localStream.setVideoProfile(cameraVideoProfile);
-
-//     // The user has granted access to the camera and mic.
-//     localStream.on("accessAllowed", function() {
-//       if(devices.cameras.length === 0 && devices.mics.length === 0) {
-// 				console.log('[DEBUG] : checking for cameras & mics');
-//         getCameraDevices();
-//         getMicDevices();
-//       }
-//       console.log("accessAllowed");
-//     });
-//     // The user has denied access to the camera and mic.
-//     localStream.on("accessDenied", function() {
-//       console.log("accessDenied");
-//     });
-
-//     localStream.init(function() {
-//       console.log('getUserMedia successfully');
-//       localStream.play('full-screen-video'); // play the local stream on the main div
-//       // publish local stream
-
-//       if($.isEmptyObject(localStreams.camera.stream)) {
-//         enableUiControls(localStream); // move after testing
-//       } else {
-//         //reset controls
-//         $("#mic-btn").prop("disabled", false);
-//         $("#video-btn").prop("disabled", false);
-//         $("#exit-btn").prop("disabled", false);
-//       }
-
-//       client.publish(localStream, function (err) {
-//         console.log('[ERROR] : publish local stream error: ' + err);
-//       });
-
-//       localStreams.camera.stream = localStream; // keep track of the camera stream for later
-//     }, function (err) {
-//       console.log('[ERROR] : getUserMedia failed', err);
-//     });
-// 	}
-},
-
-}
+      leaveChannel() {
+        this.client.leave(function() {
+          console.log('this.client leaves channel');
+          this.localStream.stop() // stop the camera stream playback
+          this.localStream.close(); // clean up and close the camera stream
+          this.client.unpublish(this.localStream); // unpublish the camera stream
+          //disable the UI elements
+          $('#mic-btn').prop('disabled', true);
+          $('#video-btn').prop('disabled', true);
+          $('#exit-btn').prop('disabled', true);
+          $("#add-rtmp-btn").prop("disabled", true);
+          $("#rtmp-config-btn").prop("disabled", true);
+        }, function(err) {
+          console.log('this.client leave failed ', err); //error handling
+        });
+        }
+      },
+  }
 </script>
 
 <style scoped>
@@ -305,8 +213,8 @@ export default {
     justify-content: center;
   }
   #video {
-    width: 200px;
-    height: 200px;
+    width: 640px;
+    height: 480px;
   }
   .player {
    border: 1px black solid;
@@ -321,9 +229,11 @@ export default {
   }
    .professional {
     position: absolute;
-    /* border: 2px solid; */
-    width: 200px;
-    height: 250px;
+    border: 2px solid;
+    width: auto;
+    height: auto;
+    display: grid;
+    grid-column: auto auto auto;
     border-width: 2px;
     border-radius: 6px;
   }
